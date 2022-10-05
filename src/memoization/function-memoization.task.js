7f9f06e3fd20b1b8838fe6aec3fe745941507136
attilagyongyosi <attilagyongyosi0@gmail.com>
2022-10-05 19:43:54 +0100

    feat: documenting memoize() task, writing unit tests

--- src/memoization/function-memoization.task.js
+++ src/memoization/function-memoization.task.js
@@ -5,12 +5,12 @@
  *
  * @param fn a function to create a memoized equivalent from.
  *
+ * @throws an error when a non-function first parameter is provided.
+ *
  * @example
  * const sum = (a, b) => a + b;
  * const memoizedSum = memoize(sum);
  * const firstSum = memoizedSum(1, 2); // result is calculated, original sum() is called
  * const secondSum = memoizedSum(1, 2); // result coming from cache
  */
-export function memoize(fn) {
-    // @TODO: implement function.
-}
+export function memoize(fn) {}
--- src/memoization/function-memoization.test.js
+++ src/memoization/function-memoization.test.js
@@ -1,5 +1,47 @@
+import { memoize } from './function-memoization.task';
+
 describe('memoize()', () => {
-    it('should', () => {
-        expect(1).to.eq(1);
+    const sum = vi.fn((...n) => n.reduce((sum, number) => sum + number));
+
+    it('should accept a function parameter and return with another function', () => {
+        const memoizedSum = memoize(sum);
+        expect(memoizedSum).toBeTypeOf('function');
+    });
+
+    it('should throw when non-function parameter is provided', () => {
+        const expectedErrorMessage = 'Invalid parameter! Please provide a callback function.';
+        expect(() => memoize(12)).toThrowError(expectedErrorMessage);
+        expect(() => memoize('string')).toThrowError(expectedErrorMessage);
+        expect(() => memoize(true)).toThrowError(expectedErrorMessage);
+        expect(() => memoize(new Date())).toThrowError(expectedErrorMessage);
+        expect(() => memoize()).toThrowError(expectedErrorMessage);
+    });
+
+    it('should memoize a sum function and cache result of first execution with given parameters', () => {
+        const parameters = [ 1, 2 ];
+        const memoizedSum = memoize(sum);
+
+        memoizedSum(...parameters);
+        memoizedSum(...parameters);
+        memoizedSum(...parameters);
+
+        expect(sum).toHaveBeenCalledWith(...parameters);
+        expect(sum).not.toHaveBeenCalledWith(1);
+    });
+
+    it('should memoize multiple set of parameters for a n-sum function', () => {
+        const parameters1 = [ 1, 3, 4 ];
+        const parameters2 = [ 1, 2, 4, 6 ];
+
+        const memoizedSum = memoize(sum);
+
+        memoizedSum(...parameters1);
+        memoizedSum(...parameters1);
+        memoizedSum(...parameters2);
+        memoizedSum(...parameters2);
+
+        expect(sum).toHaveBeenCalledWith(...parameters1);
+        expect(sum).toHaveBeenCalledWith(...parameters2);
+        expect(sum).not.toHaveBeenCalledWith(2);
     });
 });
